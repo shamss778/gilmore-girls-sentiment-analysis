058b73df3bec9ceed51eea05181812bf50e7b888
Shams Ben Mefteh <shams.ben.mefteh5@gmail.com>
2025-02-21 13:23:37 +0100

    converted pdf to cvs

---
+++ convert.py
@@ -0,0 +1,36 @@
+import PyPDF2
+import csv
+import os
+
+
+def pdf_to_csv(pdf_path, csv_path):
+    with open(pdf_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+
+        with open(csv_path, "w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["Page", "Line", "Text"])
+
+            for page_num in range(len(reader.pages)):
+                page = reader.pages[page_num]
+                text = page.extract_text()
+                lines = text.split("\n")
+
+                for line_num, line in enumerate(lines, 1):
+                    writer.writerow([page_num + 1, line_num, line.strip()])
+
+    print(f"PDF converted to CSV: {csv_path}")
+
+
+# Usage
+pdf_folder = "PDF"
+output_folder = "output"
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(pdf_folder):
+    if filename.endswith(".pdf"):
+        pdf_path = os.path.join(pdf_folder, filename)
+        csv_path = os.path.join(output_folder, f"{os.path.splitext(filename)[0]}.csv")
+        pdf_to_csv(pdf_path, csv_path)
---
+++ preprocess/seperate_columns.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+
+# Specify the folder path containing your CSV files
+folder_path = 'C:\Users\asus\Desktop\Gilmore girls sentiment analysis\CSV'
+
+# Initialize a dictionary to store DataFrames
+dataframes = {}
+
+# Iterate over CSV files in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith('.csv'):
+        file_path = os.path.join(folder_path, filename)
+        
+        # Read the CSV file into a DataFrame
+        df = pd.read_csv(file_path)
+        
+        # Drop 'Line' and 'Page' columns
+        df = df.drop(['Line', 'Page'], axis=1)
+        
+        # Store the DataFrame in the dictionary, using the filename as the key
+        dataframes[filename] = df
+
+# Now you can access each DataFrame using the filename as the key
+for filename, df in dataframes.items():
+    print(f"DataFrame for {filename}:")
+    print(df)
+    print("\n")
